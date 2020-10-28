1e37b49b5676c02aed1e16f371e1655c49667b01
LeeAhYeong <lay0916@naver.com>
2020-10-29 07:54:44 +0900

    exercise_problem

---
+++ chapter01/10_turtle_graphics_4.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.textinput("비밀번호 입력", "비밀번호를 입력해주세요")
+turtle.write("안녕하세요? 홍길동씨, 터틀 인사드립니다.")
+
+turtle.goto(0,50)
+turtle.goto(50,50)
+turtle.goto(50,0)
+turtle.goto(0,0)
+
+turtle.done()
