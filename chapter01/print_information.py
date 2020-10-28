cc927b732442e7c298a089d37620d1494b60d0f5
LeeAhYeong <lay0916@naver.com>
2020-10-28 21:05:30 +0900

    p8_ex1

---
+++ chapter01/print_information.py
@@ -0,0 +1,14 @@
+place = input('경기장은 어디입니까? ')
+winner = input('이긴 팀은 어디입니까? ')
+loser = input('진 팀은 어디입니까? ')
+good_player = input('우수선수는 누구입니까? ')
+score = input('스코어는 몇대몇입니까? ')
+
+print('='*50)
+
+print('오늘',place,'에서','야구 경기가 열렸습니다.')
+print(winner,'과',loser,'은 치열한 공방전을 펼쳤습니다.')
+print(good_player,'이 맹활약을 하였습니다.')
+print('결국',winner,'이(가)',loser,'을(를)',score,'로 이겼습니다.')
+
+print('='*50)
